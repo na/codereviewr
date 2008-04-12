a7fc320a52083e6e3f1b32f3140645677d113ff6
Nate Anderson <nande12@gmail.com>
2008-04-12 12:15:28 -0700

    updated urls.py for comments

--- codereviewr/code/urls.py
+++ codereviewr/code/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
-from codereviewr.code.views import code_add, code_list, code_detail, code_comment
+from codereviewr.code.views import code_add, code_list, code_detail, code_comments
 
 urlpatterns = patterns('',
     url(r'^$', code_list, name='code_list'),
     url(r'^add/$', code_add, name='code_add'),
     url(r'^(?P<code_id>\d+)/$', code_detail, name='code_detail'),
     url(r'^(?P<code_id>\d+)/(?P<compare_to_parent>diff)/$',code_detail,name='code_diff'),
-    url(r'^(?P<code_id>\d+/comment/\d+)/$', code_comment, name='code_comment'),
+    url(r'^(?P<code_id>\d+)/comments/$', code_comments, name='code_comments'),
 )
