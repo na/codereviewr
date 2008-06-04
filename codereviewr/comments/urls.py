272f82f4d4215bf44cc2b35361761937ce760d22
Nate Anderson <nande12@gmail.com>
2008-06-03 19:52:49 -0700

    Removing comments app commenting is in code app.

--- codereviewr/comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from codereviewr.comments.views import *
-
-urlpatterns = patterns('',
-	url(r'^comments/$', comments, name='comments'),
-	url(r'^comments/line/(?P<line_no>\d+)$', line_comments, name='line_comments'),
-)
